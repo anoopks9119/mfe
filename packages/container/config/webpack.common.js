768d189c4bcb97ad820567ec352b145139b24a4b
anoopks9119 <anoopks9119@gmail.com>
2022-07-22 14:09:32 +0530

    Created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,19 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    module : {
-        rules : [
+    module: {
+        rules: [
             {
                 test: /\.m?js$/,
-                exclude : /node_modules/,
-                use : {
+                exclude: /node_modules/,
+                use: {
                     loader: 'babel-loader',
-                    options : {
-                        presets : ['@babel/preset-react',
-                        '@babel/preset-env'
-                    ],
-                    plugins : ['@babel/plugin-transform-runtime']
+                    options: {
+                        presets: ['@babel/preset-react',
+                            '@babel/preset-env'
+                        ],
+                        plugins: ['@babel/plugin-transform-runtime']
                     }
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })]
 }

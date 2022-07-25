1a07162ca9830d96036cffed56af98bdb756850c
anoopks9119 <anoopks9119@gmail.com>
2022-07-25 12:12:44 +0530

    AUth project added and wired to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,20 +1,32 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
+import React, { Suspense, lazy, useState } from "react";
 import Header from "./components/Header";
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const generateClassName = createGenerateClassName({
     productionPrefix: "co"
-})
+});
+
+const AuthAppLAzy = lazy(() => import("./components/AuthApp"));
+const MarketingAppLazy = lazy(() => import("./components/MarketingApp"));
 
 export default (() => {
+
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                    <Header />
-                    <MarketingApp />
+                    <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn}/>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route path="/auth">
+                                <AuthAppLAzy onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
+                            <Route path="/" component={MarketingAppLazy} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,13 +1,14 @@
-import { mount } from "marketing/MarketingApp";
+import { mount } from "auth/AuthApp";
 import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+export default ({onSignIn}) => {
 
     const ref = useRef(null);
     const history = useHistory();
 
     useEffect(() => {
+
         const { onParentNavigate } = mount(ref.current, {
             initialPath : history.location.pathname,
             onNavigate: ({ pathname: nextPathname }) => {
@@ -15,7 +16,8 @@ export default () => {
                 if (pathname !== nextPathname) {
                     history.push(nextPathname);
                 }
-            }
+            },
+            onSignIn
         });
 
         history.listen(onParentNavigate);
